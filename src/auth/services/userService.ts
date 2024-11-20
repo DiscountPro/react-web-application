a4439f0a13a4e0693f3a97389e242d418cdab9d5
SgtGarro <marcelogarro137@gmail.com>
2024-11-19 21:21:28 -0500

    fix: change user service url for prod service

--- src/auth/services/userService.ts
+++ src/auth/services/userService.ts
@@ -2,7 +2,8 @@ import axios from 'axios'
 import { User } from '../model/user'
 
 export class UserService {
-  private static baseURL = 'http://localhost:3000/api/v1/users'
+  private static baseURL =
+    import.meta.env.VITE_API_URL ?? 'http://localhost:3000/api/v1/users'
 
   static async register(user: Omit<User, 'id'>) {
     const response = await axios.post<User>(UserService.baseURL, user)
